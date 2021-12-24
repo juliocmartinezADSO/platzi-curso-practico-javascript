14c11764760bac27a9f5976c6fea6f2f6f2bb07c
juliocmartinez <julio.martinezg8@gmail.com>
2021-12-24 17:57:09 -0500

    Version 2

---
+++ figuras.js
@@ -0,0 +1,31 @@
+//Cuadrados
+
+console.group("Cuadrados");
+//cuando enviamos datos a través de una funcion() 
+//se denominan argumentos
+
+//Los que le pasamos a una funcion se llaman parametros
+function perimetroCuadrado(lado){
+    return lado*4;
+}
+
+function areaCuadrado(lado){
+    return lado*lado;
+}
+
+//Triangulos
+
+function perimetroTriangulo(lado1,lado2,base){
+    return lado1+lado2+base
+
+}
+
+function areaTriangulo(base,altura){
+    
+}
+
+
+
+
+
+console.groupEnd();
